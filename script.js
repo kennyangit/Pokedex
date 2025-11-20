694e6ea5e62ea196907d2ca3552336c216d77c26
Yan Kenny <kkyan092@gmail.com>
2025-11-20 19:29:55 -0300

    script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ async function loadPokemon() {
         console.error("Erro ao carregar Pokémons:", erro);
 
         list.innerHTML = `
-            <p class="text-red-500 text-center text-lg">
+            <p class="align-center text-red-500 text-center text-lg">
                 Erro ao carregar Pokémons. A PokeAPI pode estar instável.  
                 <br>Tente novamente em alguns segundos.
             </p>`;
