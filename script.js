af3fc4a817c60e4f4db4ed554fece36a9f7ff2a2
Yan Kenny <kkyan092@gmail.com>
2025-11-25 21:29:31 -0300

    final

--- script.js
+++ script.js
@@ -33,15 +33,15 @@ darkToggle.addEventListener("click", () => {
 // Cores dos tipos
 function getColor(type) {
     const colors = {
-        fire: "bg-orange-500",
-        water: "bg-blue-500",
-        grass: "bg-green-500",
-        electric: "bg-yellow-400",
-        psychic: "bg-purple-500",
-        dark: "bg-gray-600",
-        dragon: "bg-indigo-600"
+        fire: "orange",
+        water: "blue",
+        grass: "green",
+        electric: "yellow",
+        psychic: "purple",
+        dark: "gray",
+        dragon: "indigo"
     };
-    return colors[type] || "bg-gray-500";
+    return colors[type] || "gray";
 }
 
 async function getPokemonNamePT(id) {
@@ -89,7 +89,9 @@ function renderPokemon(listData) {
 
     listData.forEach(p => {
         const typesHTML = p.types.map(t =>
-            `<span class="px-2 py-1 rounded text-white text-xs ${getColor(t)}">`
+            `<span class="px-2 py-1 rounded text-white text-xs" style="background:${getColor(t)}">
+            ${t}
+            </span>`
         ).join("");
 
         list.innerHTML += `
